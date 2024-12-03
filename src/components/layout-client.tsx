5f52a535d8a433e692fdfa036b95883cfe631d49
Alandla <alandla00@gmail.com>
2024-12-04 00:14:02 +0100

    Add metrics

--- src/components/layout-client.tsx
+++ src/components/layout-client.tsx
@@ -5,11 +5,15 @@ import { getMessages } from "next-intl/server";
 import NextTopLoader from "nextjs-toploader";
 import { Toaster } from "@/src/components/ui/toaster";
 import { CrispChat } from "./crisp-chat";
+import MicrosoftClarity from "./metrics/MicrosoftClarity";
+import GoogleAnalytics from "./metrics/GoogleAnalytics";
 
 const ClientLayout = async ({ children }: { children: React.ReactNode }) => {
   const messages = await getMessages();
   return (
     <>
+      {process.env.NODE_ENV !== 'development' && <MicrosoftClarity />}
+      {process.env.NODE_ENV !== 'development' && <GoogleAnalytics />}
       <SessionProvider>
         <NextIntlClientProvider messages={messages}>
           <Toaster />
