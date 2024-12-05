582f981925505a05e49518a51ec144d604eaa3e6
Alandla <alandla00@gmail.com>
2024-12-05 05:17:44 +0100

    Add mail in beta list

--- src/config/beta.config.ts
+++ src/config/beta.config.ts
@@ -2,5 +2,15 @@ export const mailBetaList = [
     "taftingmc@gmail.com",
     "alan@hoox.video",
     "maxime@hoox.video",
-    "blablatitan@gmail.com"
+    "blablatitan@gmail.com",
+    "marie@lalogecoaching.com",
+    "thibaut.reygner@adplorer.com",
+    "erwan@vingtdeuxcinq.com",
+    "g.furrer@gfproductions.ch",
+    "canaultr@gmail.com",
+    "bukkart.studio@gmail.com",
+    "contact@christophe-guillon.com",
+    "clotilde.bouchet.coach@gmail.com",
+    "gdmgroupsarl@gmail.com",
+    "torrent.water.92@gmail.com"
 ]
