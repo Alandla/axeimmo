ff2dc36b01aa9963174edff26621390df3074861
Alandla <alandla00@gmail.com>
2024-12-08 17:09:30 +0100

    New watermark

--- src/remotion/generateVideo/components/Watermark.tsx
+++ src/remotion/generateVideo/components/Watermark.tsx
@@ -1,19 +1,20 @@
 
-import { AbsoluteFill, Img, staticFile } from "remotion";
+import { AbsoluteFill, Img, OffthreadVideo, staticFile } from "remotion";
 
 export const Watermark = () => {
  
 	return (
 		<AbsoluteFill>
-			<Img
-				src={"https://media.hoox.video/hoox-watermark.png"}
+			<OffthreadVideo
+				src={"https://media.hoox.video/Logo%20Hoox%20Animation%20(2).webm"}
+				transparent={true}
 				style={{
                     position: 'absolute', // Positions the image absolutely
-                    top: '300px', // Adds a 10px margin from the top
+                    top: '40px', // Adds a 10px margin from the top
                     left: '20px', // Adds a 10px margin from the left
-                    width: '26%', // Reduces the size to 50% of the original
+                    width: '19%', // Reduces the size to 50% of the original
                     height: 'auto', // Keeps the aspect ratio
-                    opacity: 0.7, // Adds transparency
+                    opacity: 0.85, // Adds transparency
 					zIndex: 1000,
                 }}
 			/>
