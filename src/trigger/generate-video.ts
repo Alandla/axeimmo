fb9fcca098db96b952cf10dea19996d7ab249feb
Alandla <alandla00@gmail.com>
2025-04-25 01:56:26 +0200

    Update trigger development mode

--- src/trigger/generate-video.ts
+++ src/trigger/generate-video.ts
@@ -62,7 +62,7 @@ export const generateVideoTask = task({
     const mediaSource = payload.mediaSource || "PEXELS";
     const avatarFile = payload.files.find(f => f.usage === 'avatar')
 
-    const isDevelopment = ctx.environment.type === "PRODUCTION"
+    const isDevelopment = ctx.environment.type === "DEVELOPMENT"
 
     let videoStyle: string | undefined;
     let scriptLength = payload.script ? payload.script.length : 0;
