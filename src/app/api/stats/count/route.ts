16930f019d126c4aceb41b426de65767741e07f9
Alandla <alandla00@gmail.com>
2025-04-30 03:27:09 +0200

    Add api route for getting stats on LP

---
+++ src/app/api/stats/count/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server';
+import UserModel from '@/src/models/User';
+import VideoModel from '@/src/models/Video';
+import { executeWithRetry } from '@/src/lib/db';
+
+export async function GET(req: NextRequest) {
+
+  console.log("GET /api/stats/count");
+
+  try {
+    const [userCount, videoCount] = await Promise.all([
+      executeWithRetry(async () => {
+        return await UserModel.countDocuments({});
+      }),
+      executeWithRetry(async () => {
+        return await VideoModel.countDocuments({ archived: { $ne: true } });
+      })
+    ]);
+
+    return NextResponse.json({
+      data: {
+        users: userCount,
+        videos: videoCount
+      }
+    });
+  } catch (error) {
+    console.error("Error while fetching stats:", error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+} 
