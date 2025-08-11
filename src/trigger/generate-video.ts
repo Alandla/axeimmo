a718e8941794ed7fe7018d180e1c8a6c5a3e5cd6
Alandla <alandla00@gmail.com>
2025-08-11 18:34:03 +0200

    fix error linter

--- src/trigger/generate-video.ts
+++ src/trigger/generate-video.ts
@@ -1617,7 +1617,7 @@ export const generateVideoTask = task({
 
     newVideo = {
       ...newVideo,
-      costToGenerate: cost + ctx.run.baseCostInCents,
+      costToGenerate: cost + (ctx.run.baseCostInCents || 0),
       state: {
         type: 'done',
       },
